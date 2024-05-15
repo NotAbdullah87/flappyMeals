e6786f486865731be382c39788014a2ca1bdb917
NotAbdullah87 <115102771+NotAbdullah87@users.noreply.github.com>
2024-05-15 08:39:43 -0700

    Update server.js

--- flappymeals/backend/server.js
+++ flappymeals/backend/server.js
@@ -1,35 +1,16 @@
-
-const Express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const bodyParser = require("body-parser");
-
-const app = Express();
-
-app.use(bodyParser.json()); // apply body-parser middleware
-
-
-
-
-const connString = "mongodb+srv://admin:flappy123@flappymeals.xkolew3.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=flappyMeals";
-
-// const client = new MongoClient(connString, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const PORT = 4000;
+const express = require('express')
+const app = express()
+const PORT = 4000
 
 
 app.get('/home', (req, res) => {
 res.status(200).json('Welcome, your app is working well');
 })
 
+
 app.listen(PORT, () => {
 console.log(`Server running at http://localhost:${PORT}`);
-  // await client.connect();
-console.log("Connected to MongoDB");
 });
-
-
-
-
 
 // Export the Express API
 module.exports = app 
